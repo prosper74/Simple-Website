0472b1b807c59c43bd493d4e14fc6dc936f7eaa0
Prosper Atu <atu.prosper@gmail.com>
2021-08-20 17:05:39 +0100

    Added a spinner loading screen with react spinners

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/common/Layout"
 import Banner from "../components/home/Banner"
 import About from "../components/home/About"
@@ -6,16 +6,35 @@ import Services from "../components/home/Services"
 import Projects from "../components/home/Projects"
 import HomeBlog from "../components/home/HomeBlog"
 import Contact from "../components/home/Contact"
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader"
+import "../styles/global.css"
 
 export default function Home() {
+  let [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2500)
+  }, [])
+
   return (
-    <Layout>
-      <Banner />
-      <Services />
-      <About />
-      <Projects />
-      <HomeBlog />
-      <Contact />
-    </Layout>
+    <div className="mainPage">
+      {loading ? (
+        <div className="loader">
+          <ClimbingBoxLoader color={"#6633FF"} loading={loading} size={20} />
+        </div>
+      ) : (
+        <Layout>
+          <Banner />
+          <Services />
+          <About />
+          <Projects />
+          <HomeBlog />
+          <Contact />
+        </Layout>
+      )}
+    </div>
   )
 }
